c335df5592135b512629ebf90ef508d4a97261b9
Gustav Behm <me@rootmos.io>
2025-03-21 10:57:51 +0100

    Quick'n'dirty sketch of "multiplying the stencil": WIP towards arbitrary base conversion

--- src/sandbox.lua
+++ src/sandbox.lua
@@ -1,16 +1,36 @@
 local P = require("polynomial")
 
-local a = {1, 2, 3, o=0, n=3}
-local b = {4, nil, 5, o=1, n=3, v="y"}
-local c = {7, o=8, n=1, v="z"}
-local d = {9, o=0, n=1, v="A"}
+local A = 10
+local B = 16
 
-print(string.format("a := %s", P.tostring(a)))
-print(string.format("b := %s", P.tostring(b)))
-print(string.format("c := %s", P.tostring(c)))
-print(string.format("d := %s", P.tostring(d)))
+local A2 = P.make{4, 6, v="B"}
 
-print(string.format("a + b = %s", P.tostring(P.add(a, b))))
-print(string.format("b + a = %s", P.tostring(P.add(b, a))))
-print(string.format("b + c = %s", P.tostring(P.add(b, c))))
-print(string.format("c + b = %s", P.tostring(P.add(c, b))))
+print(string.format("A^2 := %s", P.tostring(A2)))
+
+local A4 = A2*A2
+print(string.format("A^4 := %s", P.tostring(A4)))
+
+local function carry_the_one(p)
+    local i = 1
+    while true do
+        local k = p[i]
+        if k == nil then
+            break
+        end
+        if k >= B then
+            local q = k // B
+            local r = k % B
+            p[i] = r
+            p[i+1] = (p[i+1] or 0) + q
+        end
+
+        i = i + 1
+    end
+    p.n = i
+    return p
+end
+
+-- TODO local A4_p = carry_the_one(A4:clone())
+local A4_p = carry_the_one(A4)
+
+print(string.format("A^4' := %s", P.tostring(A4)))
