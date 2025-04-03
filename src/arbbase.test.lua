5089c57c8292e0ebe8f20cd539f159a64b3f1fc6
Gustav Behm <me@rootmos.io>
2025-04-03 20:38:53 +0200

    Add a naive test for the digitsInBase 16 call

--- src/arbbase.test.lua
+++ src/arbbase.test.lua
@@ -2,17 +2,28 @@ local lu = require("luaunit")
 local M = require("arbbase")
 local Ascii = require("ascii")
 
-local function run_example(a0, b0)
-    local b1 = M.dec_to_hex(Ascii.be_string_to_le_digits(a0))
-    local a1 = Ascii.le_digits_to_be_string(b1)
-    lu.assertEquals(a1, b0)
+local function run_dec_to_hex(a0, b0)
+  local b1 = M.dec_to_hex(Ascii.be_string_to_le_digits(a0))
+  local a1 = Ascii.le_digits_to_be_string(b1)
+  lu.assertEquals(a1, b0)
+end
+
+local function run_hex_to_dec(a0, b0)
+  local b1 = M.hex_to_dec(Ascii.be_string_to_le_digits(a0))
+  local a1 = Ascii.le_digits_to_be_string(b1)
+  lu.assertEquals(a1, b0)
+end
+
+local function run_example(dec, hex)
+  run_dec_to_hex(dec, hex)
+  run_hex_to_dec(hex, dec)
 end
 
 function test_example0()
-    run_example(
-        "10000000000000000000000000000000000000000000000000000000000000000000000000000000001",
-        "15159af8044462379881065d41ad19c19af0eeb576360c36400000000000000000001"
-    )
+  run_example(
+    "10000000000000000000000000000000000000000000000000000000000000000000000000000000001",
+    "15159af8044462379881065d41ad19c19af0eeb576360c36400000000000000000001"
+  )
 end
 
 local mk_a_big_number_up_my_sleave = function()
@@ -40,12 +51,10 @@ local mk_a_big_number_up_my_sleave = function()
 end
 
 function test_example1()
-    run_example(
-        mk_a_big_number_up_my_sleave(),
-             "142ffdb89886eaa285d18e52687a39b993ec2eaad4c4b1e57b8d2e8a4e91242e6030f22e3710921d04a9013af8e4a5ea76e6a2c3aae983417bc8ed44ca3e890fad50f537dc39a729e8bc02b429d72df837ae180d5779a26c9974f52c1316b1dbe5ee40475edec045a96bce696429281e9bd11d7ab13474c40f76d1b5f0fdfd0fb28572f5a31d07a80aeae2a8eca814b2b341639439dc1145a79d0ca4c7c5fcb46c16c68e73f9f11c60b4d14d477bb549ba2d3064db38227a182781b8c958fa049eb8cc1dc651e89c985fd2dc0404c0b62aa0e5de7de3aebcca28276d45f5f613e4a60c9b846dbc5c8245a5e054"
--- expected: "142ffdb89886eaa285d18e52687a39b993ec2eaad4c4b1e57b8d2e8a4e91242e6030f22e3710921d04a9013af8e4a5ea76e6a2c3aae983417bc8ed44ca3e890fad50f537dc39a729e8bc02b429d72df837ae180d5779a26c9974f52c1316b1dbe5ee40475edec045a96bce696429281e9bd11d7ab13474c40f76d1b5f0fdfd0fb28572f5a31d07a80aeae2a8eca814b2b341639439dc1145a79d0ca4c7c5fcb46c16c68e73f9f11c60b4d14d477bb549ba2d3064db38227a182781b8c958fa049eb8cc1dc651e89c985fd2dc0404c0b62aa0e5de7de3aebcca28276d45f5f613e4a60c9b846dbc5c8245a5e054"
---   actual: "142ffdb89886eaa285d18e52687a39b993ec2eaad4c4b1e57b8d2e8a4e91242e6030f22e3710921d04a9013af8e4a5ea76e6a2c3aae983417bc8ed44ca3e890fad50f537dc39a729e8bc02b429d72df837ae180d5779a26c9974f52c1316b1dbe5ee40475edec045a96bce696429281e9bd11d7ab13474c40f76d1b5f0fdfd0fb28572f5a31d07a80aeae2a8eca814b2b341639439dc1145a79d0ca4c7c5fcb46c16c68e73f9f11c60b4d14d477bb549ba2d3064db38227a182781b8c958fa049eb8cc1dc651e89c985fd2dc0404c0b62aa0e5de7de3aebcca28276d45f5f613e4a60c9b846dbc5c8245a5dffa"
-    )
+  run_example(
+    mk_a_big_number_up_my_sleave(),
+    "142ffdb89886eaa285d18e52687a39b993ec2eaad4c4b1e57b8d2e8a4e91242e6030f22e3710921d04a9013af8e4a5ea76e6a2c3aae983417bc8ed44ca3e890fad50f537dc39a729e8bc02b429d72df837ae180d5779a26c9974f52c1316b1dbe5ee40475edec045a96bce696429281e9bd11d7ab13474c40f76d1b5f0fdfd0fb28572f5a31d07a80aeae2a8eca814b2b341639439dc1145a79d0ca4c7c5fcb46c16c68e73f9f11c60b4d14d477bb549ba2d3064db38227a182781b8c958fa049eb8cc1dc651e89c985fd2dc0404c0b62aa0e5de7de3aebcca28276d45f5f613e4a60c9b846dbc5c8245a5e054"
+  )
 end
 
 os.exit(lu.LuaUnit.run())
--- tests/specs/UtilsSpec.hs
+++ tests/specs/UtilsSpec.hs
@@ -2,16 +2,21 @@ module UtilsSpec where
 
 import Test.Hspec
 import Test.QuickCheck
+import Text.Printf
+import Data.Char ( digitToInt )
 
 import Utils
 
 spec :: Spec
 spec = do
   describe "Utils" $ do
-   describe "digitsInBase" $ do
-    it "should render decimals" $ properly $ \(Positive n) ->
-      (concat $ show <$> digitsInBase 10 n) `shouldBe` (show n)
+    describe "digitsInBase" $ do
+      it "should render decimal digits" $ properly $ \(Positive n) ->
+        (concat $ show <$> digitsInBase 10 n) `shouldBe` (show n)
 
-   it "should survive a digitsInBase and evalInBase roundtrip" $ properly $ do
-      forAll (arbitrary `suchThat` ((> 1) . fst)) $ \(b, Positive n) ->
-        (evalInBase b $ reverse $ digitsInBase b n) `shouldBe` n
+      it "should render hexadecimal digits" $ properly $ \(Positive n) ->
+        (digitsInBase 16 n) `shouldBe` (fromIntegral . digitToInt <$> printf "%x" n)
+
+  it "(evalInBase b $ reverse $ digitsInBase b n) should be n" $ properly $ do
+    forAll (arbitrary `suchThat` ((> 1) . fst)) $ \(b, Positive n) ->
+      (evalInBase b $ reverse $ digitsInBase b n) `shouldBe` n
