719ce801ac44d85d89bb66e8ba38cf439123fcbb
Gustav Behm <me@rootmos.io>
2025-04-04 09:59:41 +0200

    Use the Huge integers when testing the Utils

--- tests/specs/UtilsSpec.hs
+++ tests/specs/UtilsSpec.hs
@@ -6,17 +6,18 @@ import Text.Printf
 import Data.Char ( digitToInt )
 
 import Utils
+import Huge
 
 spec :: Spec
 spec = do
   describe "Utils" $ do
     describe "digitsInBase" $ do
-      it "should render decimal digits" $ properly $ \(Positive n) ->
+      it "should render decimal digits" $ properly $ \Huge { getHuge = n}->
         (concat $ show <$> digitsInBase 10 n) `shouldBe` (show n)
 
       it "should render hexadecimal digits" $ properly $ \(Positive n) ->
         (digitsInBase 16 n) `shouldBe` (fromIntegral . digitToInt <$> printf "%x" n)
 
   it "(evalInBase b $ reverse $ digitsInBase b n) should be n" $ properly $ do
-    forAll (arbitrary `suchThat` ((> 1) . fst)) $ \(b, Positive n) ->
+    forAll (arbitrary `suchThat` ((> 1) . fst)) $ \(b, Huge { getHuge = n}) ->
       (evalInBase b $ reverse $ digitsInBase b n) `shouldBe` n
