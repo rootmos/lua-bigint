6e2f5e2d2ae54d0b3acde6be37b56dda4f31df11
Gustav Behm <me@rootmos.io>
2025-04-05 12:22:49 +0200

    Add some examples from Wolfram Alpha for the digitsInBase function

--- tests/specs/UtilsSpec.hs
+++ tests/specs/UtilsSpec.hs
@@ -18,6 +18,24 @@ spec = do
       it "should render hexadecimal digits" $ properly $ \(Positive n) ->
         (digitsInBase 16 n) `shouldBe` (reverse $ fromIntegral . digitToInt <$> printf "%x" n)
 
+      describe "examples" $ do
+        let example x b ds =
+              it (printf "digitsInBase %d %d should be %s" b x ds) $ do
+                digitsInBase b x `shouldBe` reverse (fromIntegral . digitToInt <$> ds)
+
+        -- https://www.wolframalpha.com/input?i=454367+in+base+3
+        example 454367 3 "212002021102"
+        example 454367 4 "1232323133"
+        example 454367 8 "1567337"
+        example 454367 12 "19ab3b"
+
+        -- https://www.wolframalpha.com/input?i=71826901+in+base+7
+        example 71826901 7 "1531342606"
+        example 71826901 4 "10101333313111"
+        example 71826901 8 "421776725"
+        example 71826901 12 "2007a5b1"
+        example 71826901 16 "447fdd5"
+
   it "(evalInBase b . digitsInBase b $ n) should be n" $ properly $ do
     forAll (arbitrary `suchThat` ((> 1) . fst)) $ \(b, Huge { getHuge = n}) ->
       (evalInBase b . digitsInBase b $ n) `shouldBe` n
