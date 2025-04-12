c3867539ff0e444cdb2dffa436e2913625d0bd38
Gustav Behm <me@rootmos.io>
2025-04-12 19:03:53 +0200

    Push BigNat:s

--- tests/src/BignatSpec.hs
+++ tests/src/BignatSpec.hs
@@ -2,6 +2,9 @@ module BignatSpec where
 
 import Text.Printf
 
+--import qualified Data.ByteString as BS
+import qualified Data.ByteString.UTF8 as BSUTF8
+
 import Test.Hspec
 import Test.QuickCheck
 
@@ -23,6 +26,31 @@ runAndPeek = mkRunAndPeek runLua
 evalAndPeek :: EvalLuaAndPeek
 evalAndPeek = mkEvalAndPeek runAndPeek
 
+newtype BigNat = N Integer deriving ( Show, Eq )
+
+instance Arbitrary BigNat where
+  arbitrary = N . getNonNegative <$> arbitrary
+
+pushBigNat :: LuaError e => BigNat -> LuaE e ()
+pushBigNat (N n) = ensureStackDiff 1 $ do
+  OK <- dostring $ BSUTF8.fromString $ printf "return M.fromstring('%s')" (show n)
+  return ()
+
+withBigNats :: [ (Name, BigNat) ] -> RunLuaAndPeek
+withBigNats ps ls = runLua $ stackNeutral $ do
+  flip mapM_ ps $ \(n, b) -> stackNeutral $ do
+    pushBigNat b
+    setglobal n
+
+  flip mapM_ ls $ \l -> dostring (BSUTF8.fromString l) >>= \case
+    OK -> return ()
+    ErrRun -> throwErrorAsException
+    _ -> undefined
+
+  a <- peek top
+  pop 1
+  return a
+
 spec :: Spec
 spec = do
   describe "bignat.lua" $ do
@@ -41,3 +69,6 @@ spec = do
       evalAndPeek (printf "M.fromstring('%s'):tostring()" (show n)) >>= flip shouldBe (show n)
     it "build and reproduce decimal strings of huge integers" $ properly $ \(Huge { getHuge = n }) ->
       evalAndPeek (printf "M.fromstring('%s'):tostring()" (show n)) >>= flip shouldBe (show n)
+
+    it "should produce decimal strings of integers pushed from Haskell" $ properly $ \(b@(N n)) ->
+      withBigNats [ ("a", b) ] [ "return a:tostring()" ] >>= flip shouldBe (show n)
