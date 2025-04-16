b792c3b862534eba9b5e295964ae793f8d9c086f
Gustav Behm <me@rootmos.io>
2025-04-16 17:12:50 +0200

    Begin following the wikipedia example

--- src/sandbox.lua
+++ src/sandbox.lua
@@ -3,6 +3,40 @@ local A = require"arbbase"
 local N = require"bignat"
 local I = require"internal"
 
-local y = 3
-local cmp = function(x) if x == y then return 0 elseif x < y then return 1 else return -1 end end
-print(I.binsearch(0, 9, cmp))
+N.default_base = 10
+
+local a = N.fromstring"1260257"
+local b = N.fromstring"37"
+
+assert(a.base == b.base)
+local B = N.make{0,1}
+
+assert(a.o == 0)
+assert(b.o == 0)
+
+local k = a.o + a.n
+local l = b.o + b.n
+print(string.format("k=%d l=%d", k, l))
+
+local function alpha(i)
+    return a[a.n - i]
+end
+
+local function beta(i)
+    return b[b.n - i]
+end
+
+local q = N.make{0}
+print(string.format("q_{-1}=%s", q))
+
+local r = N.make{0}
+for i = 0,l-2 do
+    r = r + N.make{alpha(i), o=l-2-i}
+end
+print(string.format("r_{-1}=%s", r))
+
+local i = 0
+print(string.format("i=%d", i))
+
+local di = B*r + N.make{alpha(i + l - 1)}
+print(di)
