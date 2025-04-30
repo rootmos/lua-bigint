d176c704bafb85bba9dd6ebdb6dfcfa3e4658bff
Gustav Behm <me@rootmos.io>
2025-04-30 13:48:45 +0200

    Generate and push huge integers

--- tests/src/Bignat2Spec.hs
+++ tests/src/Bignat2Spec.hs
@@ -29,6 +29,7 @@ data Operand = OpI (Maybe Base) Integer
 
 operandToInteger :: Operand -> Integer
 operandToInteger (OpI _ i) = i
+operandToInteger (OpH _ h) = getHuge h
 operandToInteger _ = undefined
 
 instance Eq Operand where
@@ -41,7 +42,7 @@ instance Peekable Operand where
       peekIndexRaw i (\idx -> fromMaybe 0 <$> peekNilOr peekIntegral idx) idx
     o :: Integer <- peekFieldRaw peekIntegral "o" idx
     b <- peekFieldRaw peekIntegral "base" idx
-    return $ OpI (Just b) . (* b^o) $ evalInBase b as
+    return $ OpI (Just b) $ (* b^o) $ evalInBase b as
 
 maxBase :: Integer
 maxBase = 2^(case luaBits of { Lua32 -> 15 :: Integer; Lua64 -> 31 })
@@ -50,22 +51,18 @@ genBase :: Gen (Maybe Base)
 genBase = oneof [ return Nothing, Just <$> chooseInteger (2, maxBase) ]
 
 instance Arbitrary Operand where
-  arbitrary = do
-    b <- genBase
-    OpI b . getNonNegative <$> arbitrary
+  arbitrary = frequency [ (60, genI)
+                        , (40, genH)
+                        ]
+    where genI = OpI <$> genBase <*> (getNonNegative <$> arbitrary)
+          genH = OpH <$> genBase <*> arbitrary
 
 instance Pushable Operand where
   push (OpI Nothing i) = dostring' (printf "return N.fromstring('%s')" (show i))
   push (OpI (Just b) i) = dostring' (printf "return N.fromstring('%s', 10, %d)" (show i) b)
+  push (OpH Nothing h) = dostring' (printf "return N.fromstring('%s')" (show $ getHuge h))
+  push (OpH (Just b) h) = dostring' (printf "return N.fromstring('%s', 10, %d)" (show $ getHuge h) b)
   push _ = undefined
-
--- with some invariants:
--- push:ing defaults Nothing to M.default_base
--- peek:ing always sets base, and never produce H3?
--- comparison/conversion doesn't use the maybe base in I3 and H3
-
--- gen coin-flips: I3,H3 Nothing or random [2,max_base]
--- gen coin-flips: Z3 max_base or random [2,max_base]
 
 spec :: Spec
 spec = describe "Bignat2" $ do
